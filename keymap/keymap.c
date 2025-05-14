31800fcd48c0d9c267887acb993ca0f13ab70aff
A1nz2802 <a1nzdev28@gmail.com>
2025-05-13 19:30:54 -0500

    fix oled render and update keymap

--- keymap/keymap.c
+++ keymap/keymap.c
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 #include <stdio.h>
+#include "quantum.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x6_3(
@@ -26,21 +27,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_TAB,    KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                         KC_D,    KC_H,    KC_T,    KC_N,    KC_S,   MO(3),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_QUOT,    KC_Q,    KC_J,    KC_K,    KC_X,                         KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_RSFT,
+      KC_LSFT, KC_QUOT,    KC_Q,    KC_J,    KC_K,    KC_X,                         KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, KC_LCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(1),  KC_ENT,     KC_SPC,   MO(2), KC_RALT
+                                          KC_LGUI,   MO(1),  KC_ENT,     KC_SPC,   MO(2), KC_LALT
                                       //`--------------------------'  `--------------------------'
   ),
 
     [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC, XXXXXXX, XXXXXXX, KC_PSCR, KC_SCRL, KC_PAUS,                      XXXXXXX, XXXXXXX,   KC_UP, XXXXXXX, XXXXXXX, XXXXXXX,
+       KC_ESC,  KC_INS, XXXXXXX, KC_PSCR, KC_SCRL, KC_PAUS,                      XXXXXXX, XXXXXXX,   KC_UP, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_TAB, XXXXXXX, KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI,                      KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
+       KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX,  KC_INS, KC_HOME, KC_PGUP,                      XXXXXXX,  KC_DEL,  KC_END, KC_PGDN, XXXXXXX, KC_RSFT,
+      KC_LSFT, XXXXXXX, XXXXXXX, KC_PGUP, KC_PGDN, XXXXXXX,                      XXXXXXX,  KC_DEL, KC_HOME,  KC_END, XXXXXXX, KC_LCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI, XXXXXXX,  KC_ENT,     KC_SPC, XXXXXXX, KC_RALT
+                                          KC_LGUI, XXXXXXX,  KC_ENT,     KC_SPC, XXXXXXX, KC_LALT
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -50,9 +51,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_TAB, XXXXXXX, KC_SLSH,   KC_AT, KC_CIRC, KC_DQUO,                      KC_EXLM, KC_LPRN, KC_RPRN, KC_DQUO, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, KC_PLUS, KC_MINS, KC_UNDS,                      KC_QUES, KC_LCBR, KC_RCBR, KC_ASTR, XXXXXXX, KC_RSFT,
+      KC_LSFT, XXXXXXX, XXXXXXX, KC_PLUS, KC_MINS, KC_UNDS,                      KC_QUES, KC_LCBR, KC_RCBR, KC_ASTR, XXXXXXX, KC_LCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(4),  KC_ENT,     KC_SPC, XXXXXXX, KC_RALT
+                                          KC_LGUI,   MO(4),  KC_ENT,     KC_SPC, XXXXXXX, KC_LALT
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -62,9 +63,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_TAB, XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8,                      XXXXXXX,    KC_4,    KC_5,    KC_6, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,                      XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_0, KC_RSFT,
+      KC_LSFT, XXXXXXX,   KC_F1,   KC_F2,   KC_F3,   KC_F4,                      XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_0, KC_LCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,   MO(4),  KC_ENT,     KC_SPC, XXXXXXX, KC_RALT
+                                          KC_LGUI,   MO(4),  KC_ENT,     KC_SPC, XXXXXXX, KC_LALT
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -72,50 +73,126 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       QK_BOOT, XXXXXXX,RGB_RMOD, RGB_MOD, RGB_VAI, RGB_VAD,                      RGB_M_P, RGB_M_B, RGB_M_R,RGB_M_SW, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, XXXXXXX, XXXXXXX, XXXXXXX, RGB_SAI, RGB_SAD,                     RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, XXXXXXX, XXXXXXX,
+      RGB_TOG, XXXXXXX, BL_STEP, BL_BRTG, RGB_SAI, RGB_SAD,                     RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RGB_HUI, RGB_HUD,                      RGB_M_T,   BL_UP, BL_DOWN, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX, XXXXXXX, RGB_SPI, RGB_SPD, RGB_HUI, RGB_HUD,                      RGB_M_T,   BL_UP, BL_DOWN, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX, XXXXXXX, XXXXXXX,    KC_TRNS, XXXXXXX, XXXXXXX
                                       //`--------------------------'  `--------------------------'
   )
 };
 
-#ifdef OLED_DRIVER_ENABLE
+#ifdef OLED_ENABLE
+
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   if (!is_keyboard_master()) {
-    return OLED_ROTATION_180;
+    return OLED_ROTATION_180; // flips the display 180 degrees if offhand
   }
   return rotation;
 }
-#endif
 
-#define L_DVORAK 0
-#define L_NAVIGATE 2
-#define L_SYMBOLS 4
-#define L_NUMPAD 8
-#define L_ADJUST 16
-
-void oled_render_layer_state(void) {
+static void oled_render_layer_state(void) {
   oled_write_P(PSTR("Layer: "), false);
-  switch (layer_state) {
-    case L_DVORAK:
+  switch (get_highest_layer(layer_state)) {
+    case 0:
       oled_write_ln_P(PSTR("Dvorak"), false);
       break;
-    case L_NAVIGATE:
+    case 1:
       oled_write_ln_P(PSTR("Navigate"), false);
       break;
-    case L_SYMBOLS:
+    case 2:
       oled_write_ln_P(PSTR("Symbols"), false);
       break;
-    case L_NUMPAD:
+    case 3:
       oled_write_ln_P(PSTR("Numpad"), false);
       break;
-    case L_ADJUST:
-    case L_ADJUST|L_NAVIGATE:
-    case L_ADJUST|L_SYMBOLS:
-    case L_ADJUST|L_NAVIGATE|L_SYMBOLS:
-      oled_write_ln_P(PSTR("Adjust"), false);
+    case 4:
+      oled_write_ln_P(PSTR("RGB"), false);
+      break;
+    default:
+      oled_write_ln_P(PSTR("Undef"), false);
       break;
   }
 }
+
+static char     key_name = ' ';
+static uint16_t last_keycode;
+static uint8_t  last_row;
+static uint8_t  last_col;
+
+static const char PROGMEM code_to_name[60] = {' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\', '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
+
+static void set_keylog(uint16_t keycode, keyrecord_t *record) {
+  // save the row and column (useful even if we can't find a keycode to show)
+  last_row = record->event.key.row;
+  last_col = record->event.key.col;
+
+  key_name     = ' ';
+  last_keycode = keycode;
+  if (IS_QK_MOD_TAP(keycode)) {
+    if (record->tap.count) {
+      keycode = QK_MOD_TAP_GET_TAP_KEYCODE(keycode);
+    } else {
+      keycode = 0xE0 + biton(QK_MOD_TAP_GET_MODS(keycode) & 0xF) + biton(QK_MOD_TAP_GET_MODS(keycode) & 0x10);
+    }
+  } else if (IS_QK_LAYER_TAP(keycode) && record->tap.count) {
+    keycode = QK_LAYER_TAP_GET_TAP_KEYCODE(keycode);
+  } else if (IS_QK_MODS(keycode)) {
+    keycode = QK_MODS_GET_BASIC_KEYCODE(keycode);
+  } else if (IS_QK_ONE_SHOT_MOD(keycode)) {
+    keycode = 0xE0 + biton(QK_ONE_SHOT_MOD_GET_MODS(keycode) & 0xF) + biton(QK_ONE_SHOT_MOD_GET_MODS(keycode) & 0x10);
+  }
+  if (keycode > ARRAY_SIZE(code_to_name)) {
+    return;
+  }
+
+  // update keylog
+  key_name = pgm_read_byte(&code_to_name[keycode]);
+}
+
+static const char *depad_str(const char *depad_str, char depad_char) {
+  while (*depad_str == depad_char)
+    ++depad_str;
+  return depad_str;
+}
+
+static void oled_render_keylog(void) {
+  oled_write_char('0' + last_row, false);
+  oled_write_P(PSTR("x"), false);
+  oled_write_char('0' + last_col, false);
+  oled_write_P(PSTR(", k"), false);
+  const char *last_keycode_str = get_u16_str(last_keycode, ' ');
+  oled_write(depad_str(last_keycode_str, ' '), false);
+  oled_write_P(PSTR(":"), false);
+  oled_write_char(key_name, false);
+  oled_advance_page(true);
+}
+
+__attribute__((weak)) void oled_render_logo(void) {
+    // clang-format off
+  static const char PROGMEM crkbd_logo[] = {
+    0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
+    0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
+    0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
+    0};
+  // clang-format on
+  oled_write_P(crkbd_logo, false);
+}
+
+bool oled_task_user(void) {
+  if (is_keyboard_master()) {
+    oled_render_layer_state();
+    oled_render_keylog();
+  } else {
+    oled_render_logo();
+  }
+  return false;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    set_keylog(keycode, record);
+  }
+  return true;
+}
+#endif // OLED_ENABLE
